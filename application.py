d514e0623408219ad692c977a20cdb7131619b6c
Piyush0630 <135303363+Piyush0630@users.noreply.github.com>
2025-11-16 22:37:32 +0530

    Fix return message in home route

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def home():
-    return "Flask app deployed on AWS Elastic Beanstalk! - Glad to see that"
+    return "Flask app deployed on AWS Elastic Beanstalk!"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=5000)
